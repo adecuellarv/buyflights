b4d8589abe80e3919417a41b2029f2b04a8014ae
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2021-12-17 14:28:45 -0600

    se agrega header

---
+++ src/components/common/header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBag, faUserAlt } from "@fortawesome/free-solid-svg-icons";
+import './styles.scss';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="div-home-hd">
+                <Link to="/">Inicio</Link>
+            </div>
+            <div className="cart-div">
+                <FontAwesomeIcon icon={faShoppingBag} className="icon-cart-hd" />
+            </div>
+        </div>
+    )
+};
+
+export default Header;
--- src/components/dome/dome.jsx
+++ src/components/dome/dome.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from "../common";
 import Home from "../home";
 import Results from "../results";
 
 const Dome = () => {
     return (
         <Router>
+            <Header />
             <Route path="/" exact >
                 <Home />
             </Route>
