91b15f57da24418c4770b4bf92e0fe19aa65cdad
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2021-12-16 22:51:12 -0600

    react router

--- src/components/dome/dome.jsx
+++ src/components/dome/dome.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from "../home";
 
 const Dome = () => {
-    return(<h1>Holiii</h1>);
+    return (
+        <Router>
+            <Route path="/" exact >
+                <Home />
+            </Route>
+        </Router>
+    );
 };
 
 export default Dome;
